53ab9a25dc6a6fb16e7b453d406b86b064bf808a
Praveenjhas <b23454@students.iitmandi.ac.in>
2025-09-27 14:32:16 +0530

    recursion

---
+++ recursion/fibonacci.java
@@ -0,0 +1,14 @@
+
+public class fibonacci {
+
+    static int fib(int n){
+      if(n==0)return 0;//base case
+      if(n==1)return 1;//base case
+      return fib(n-1)+fib(n-2);//recursive condtion
+}
+
+     public static void main(String[] args) {
+        System.out.println(fib(5));
+          
+     }
+}
